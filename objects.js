ee5ef9b31acb775b082532875911832f03b0f9d7
Wei Xin Koh <e0201350@u.nus.edu>
2020-03-31 19:28:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,9 @@
-var playlist = {'Eric Chou': 'This Is Love'};
+var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
+
+function updatePlaylist(playlist, artist, song) {
+  return Object.assign({}, playlist, {artist: song});
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+}
